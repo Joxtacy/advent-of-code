443da242a9651d598f67b1b809f456e9866940b4
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2022-12-02 14:29:27 +0900

    chore(rust): rewrite main

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,11 +1,19 @@
 mod day1;
+mod day2;
 
 fn main() {
     let arg = std::env::args().last().expect("Y U NO ARGS?!");
 
     match arg.as_str() {
-        "day1" => day1::run(),
-        "day1-2" => day1::run2(),
+        "day1" => {
+            day1::run("./src/day1/input");
+        }
+        "day1-2" => {
+            day1::run2("./src/day1/input");
+        }
+        "day2" => {
+            day2::run("./src/day2/input");
+        }
         _ => eprintln!("Try again (:"),
     }
 }
